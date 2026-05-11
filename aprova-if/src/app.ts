a298e84abd7640a96b37d6b8097ad72eade6812a
everton089 <evertonjusto89@gmail.com>
2026-05-11 17:35:45 +0000

    ..

---
+++ aprova-if/src/app.ts
@@ -0,0 +1,19 @@
+export default class App {
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    console.log('AprovaIF app initialized')
+    this.setupRoutes()
+    this.setupComponents()
+  }
+
+  setupRoutes() {
+    // Setup routes here
+  }
+
+  setupComponents() {
+    // Setup components here
+  }
+}
